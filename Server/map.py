9838ef1f47bfcda11ceb9564444fe9d3ee62ac0a
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-03-02 15:01:12 -0500

    Made the Pipfiles

---
+++ Server/map.py
@@ -0,0 +1,11 @@
+from time import sleep
+from actions import *
+
+flip_directions = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+traversal_path = []
+rooms_visited = 1
+data = get_init()
+
+while rooms_visited < 500:
+    # Do a whole bunch of stuff
+    pass
