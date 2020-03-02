b57f66e22bc53ef2973e39807d9351aeeb1f7249
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-03-02 18:20:40 -0500

    Started to write traversal loop and stopped because I'm not sure it's right.

--- Server/map.py
+++ Server/map.py
@@ -7,5 +7,10 @@ rooms_visited = 1
 data = get_init()
 
 while rooms_visited < 500:
-    # Do a whole bunch of stuff
-    pass
+    current_room = data['room_id']
+    room_details = get_room_info(data)
+
+    for key in room_data:
+        rooms[current_room][key] = room_data[key]
+
+# Stopping here because the logic looks pretty complex and I'd like help
